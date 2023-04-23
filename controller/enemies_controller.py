f3e14a0f262b0de3d6dfad885d6ee736ed9c4d4f
capitanFlint <tekkengod@list.ru>
2023-04-23 22:12:03 +0300

    fix enemies controller

--- controller/enemies_controller.py
+++ controller/enemies_controller.py
@@ -1,34 +1,25 @@
-from state.state import State
 from controller.controller import Controller
+from state.state import State
 
 
 class EnemiesController(Controller):
     def update_state(self, game_state: State):
-        e_move_up = True
-        e_move_down = True
-        e_move_right = True
-        e_move_left = True
-
         game_state.e_last_x = game_state.enemy_x
         game_state.e_last_y = game_state.enemy_y
-        y_up = game_state.level[game_state.enemy_y + 1][game_state.enemy_x]
-        y_down = game_state.level[game_state.enemy_y - 1][game_state.enemy_x]
-        x_right = game_state.level[game_state.enemy_y][game_state.enemy_x + 1]
-        x_left = game_state.level[game_state.enemy_y][game_state.enemy_x - 1]
-        if y_up == "#" or y_up == "^" or y_up == ">" or y_up == "<" or y_up == "v":
-            e_move_up = False
-        if y_down == "#" or y_down == "^" or y_down == ">" or y_down == "<" or y_down == "v":
-            e_move_down = False
-        if x_right == "#" or x_right == "^" or x_right == ">" or x_right == "<" or x_right == "v":
-            e_move_right = False
-        if x_left == "#" or x_left == "^" or x_left == ">" or x_left == "<" or x_left == "v":
-            e_move_left = False
 
-        if e_move_up == True and game_state.player_y > game_state.enemy_y:
-            game_state.enemy_y += 1
-        if e_move_down == True and game_state.player_y < game_state.enemy_y:
-            game_state.enemy_y -= 1
-        if e_move_right == True and game_state.player_x > game_state.enemy_x:
-            game_state.enemy_x += 1
-        if e_move_left == True and game_state.player_x < game_state.enemy_x:
-            game_state.enemy_x -= 1
+        next_y = game_state.enemy_y
+        next_x = game_state.enemy_x
+        if game_state.player_y > game_state.enemy_y:
+            next_y += 1
+        if game_state.player_y < game_state.enemy_y:
+            next_y -= 1
+        if game_state.player_x > game_state.enemy_x:
+            next_x += 1
+        if game_state.player_x < game_state.enemy_x:
+            next_x -= 1
+
+        next_cell = game_state.level[next_y][next_x]
+
+        if next_cell not in ["#", "^", ">", "<", "v"]:
+            game_state.enemy_y = next_y
+            game_state.enemy_x = next_x
