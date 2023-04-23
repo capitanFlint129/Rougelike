9ba43b1868af5e08eca53ec8a79eef44cff31e76
capitanFlint <tekkengod@list.ru>
2023-04-23 19:44:54 +0300

    add command handler

---
+++ gui/command_handler.py
@@ -0,0 +1,25 @@
+from enum import Enum
+
+import keyboard
+
+
+class UserCommand(Enum):
+    UP = 1
+    DOWN = 2
+    LEFT = 3
+    RIGHT = 4
+
+
+class CommandHandler:
+    @staticmethod
+    def get_command() -> UserCommand:
+        if keyboard.is_pressed('w'):
+            return UserCommand.UP
+        elif keyboard.is_pressed('s'):
+            return UserCommand.DOWN
+        elif keyboard.is_pressed('a'):
+            return UserCommand.LEFT
+        elif keyboard.is_pressed('d'):
+            return UserCommand.RIGHT
+        else:
+            pass
