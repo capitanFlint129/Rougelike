6caf5f452ccb7b193417b9567244d875466fd14d
ixorlive <igorshakirov43@gmail.com>
2023-05-03 20:55:19 +0300

    move Coordinates to utils package

--- state/actor.py
+++ state/actor.py
@@ -3,30 +3,7 @@ from typing import Set
 
 from state.game_object import GameObject
 from state.item import Item
-
-
-# TODO: Move to a separate file named utils.
-class Coordinates:
-    def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    def __eq__(self, other):
-        if isinstance(other, Coordinates):
-            return self.x == other.x and self.y == other.y
-        elif isinstance(other, tuple) and len(other) == 2:
-            return self.x == other[0] and self.y == other[1]
-        return False
-
-    def distance(self, other):
-        if not isinstance(other, Coordinates):
-            raise ValueError("Argument must be of type Coordinates.")
-        x_diff = self.x - other.x
-        y_diff = self.y - other.y
-        return (x_diff**2 + y_diff**2) ** 0.5
+from utils.coordinates import Coordinates
 
 
 class Actor(GameObject, ABC):
---
+++ utils/coordinates.py
@@ -0,0 +1,21 @@
+class Coordinates:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+    def __eq__(self, other):
+        if isinstance(other, Coordinates):
+            return self.x == other.x and self.y == other.y
+        elif isinstance(other, tuple) and len(other) == 2:
+            return self.x == other[0] and self.y == other[1]
+        return False
+
+    def distance(self, other):
+        if not isinstance(other, Coordinates):
+            raise ValueError("Argument must be of type Coordinates.")
+        x_diff = self.x - other.x
+        y_diff = self.y - other.y
+        return (x_diff**2 + y_diff**2) ** 0.5
