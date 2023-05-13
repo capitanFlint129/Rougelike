df28cbd09ce1c65c505d036b1b76db125e424870
capitanFlint129 <tekkengod@list.ru>
2023-05-13 22:30:45 +0300

    add docs to enemies factory

--- generators/enemies_factory.py
+++ generators/enemies_factory.py
@@ -6,6 +6,11 @@ from state.game_object import GameObject
 
 
 class EnemyFactory(ABC):
+    """An abstract base class for enemy factories that create sets of enemies.
+
+    Concrete subclasses of this class must implement the `create_enemies` method.
+    """
+
     @abstractmethod
     def create_enemies(
         self, level: int, map_array: List[List[GameObject]]
@@ -18,9 +23,20 @@ class EnemyFactory(ABC):
 
 
 class FantasyEnemyFactory(EnemyFactory):
+    """A concrete subclass of `EnemyFactory` that creates fantasy enemies."""
+
     def create_enemies(
         self, level: int, map_array: List[List[GameObject]]
     ) -> Set[Enemy]:
+        """Creates a set of fantasy enemies.
+
+        Args:
+            level: An integer representing the level of the game.
+            map_array: A list of lists representing the game map.
+
+        Returns:
+            A set of `Enemy` objects.
+        """
         x = random.randint(3, len(map_array[0]) - 3)
         y = random.randint(3, len(map_array) - 3)
         enemies_dict = {
@@ -34,9 +50,20 @@ class FantasyEnemyFactory(EnemyFactory):
 
 
 class SciFiEnemyFactory(EnemyFactory):
+    """A concrete subclass of `EnemyFactory` that creates sci-fi enemies."""
+
     def create_enemies(
         self, level: int, map_array: List[List[GameObject]]
     ) -> Set[Enemy]:
+        """Creates a set of sci-fi enemies.
+
+        Args:
+            level: An integer representing the level of the game.
+            map_array: A list of lists representing the game map.
+
+        Returns:
+            A set of `Enemy` objects.
+        """
         x = len(map_array[0]) // 2 - 2
         y = len(map_array) // 2 - 2
         enemies_dict = {
